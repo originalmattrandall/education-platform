239f8de841cd820f1e1de1228933b136eb67186c
Matt Randall <mattrandall01@gmail.com>
2022-06-04 19:57:24 -0400

    clearing components from main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { FormEventHandler } from 'react';
-import { SubmitHandler } from 'react-hook-form';
 import './App.css';
-import { LoginForm, LoginFormElements } from './presentation/components/loginform/LoginForm';
-import { VerticalNavBar } from './presentation/components/verticalnavbar/VerticalNavBar';
 
 function App() {
   return (
     <div className="App">
-      <VerticalNavBar />
-
-      <LoginForm onSubmit={onSubmit}/>
     </div>
   );
 }
 
 export default App;
-
-const onSubmit: SubmitHandler<LoginFormElements> = (event) => {
-
-  console.log(event.nameInput)
-  console.log(event.passwordInput)
-}
