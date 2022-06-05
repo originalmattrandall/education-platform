d68cf4ab9b62e7a68dead387c5a821565796a181
Matt Randall <mattrandall01@gmail.com>
2022-06-05 09:25:20 -0400

    ui overhaul

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css';
+import { LoginForm } from './presentation/components/loginform/LoginForm';
+import { VerticalNavBar } from './presentation/components/verticalnavbar/VerticalNavBar';
 
 function App() {
   return (
     <div className="App">
+      <VerticalNavBar />
+
+      <LoginForm 
+        onSubmit={(event) => {
+          console.log('form submitted')
+        }}/>
     </div>
   );
 }
